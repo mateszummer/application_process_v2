36939a662d8787445ad186757f7e8edc23e16789
mateszummer <szummerm@gmail.com>
2017-09-10 15:06:26 +0200

    task 3,4,5

--- server.py
+++ server.py
@@ -1,16 +1,43 @@
 from flask import Flask, render_template, redirect, request, session
 import sql_quaries
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def route_list():
     return render_template('index.html')
 
+
 @app.route('/mentors')
 def mentors():
     sql_list = sql_quaries.mentors_schools()
     return render_template('mentors.html', sql_list=sql_list)
+
+
+@app.route('/all-school')
+def all_school():
+    sql_list = sql_quaries.all_school()
+    return render_template('all_school.html', sql_list=sql_list)
+
+
+@app.route('/Mentors-by-country-page')
+def mentors_by_country():
+    sql_list = sql_quaries.mentors_by_country()
+    return render_template('mentors_by_country.html', sql_list=sql_list)
+
+
+@app.route('/contacts')
+def contacts():
+    sql_list = sql_quaries.contacts()
+    return render_template('contacts.html', sql_list=sql_list)
+
+
+@app.route('/applicants')
+def applicants():
+    sql_list = sql_quaries.applicants()
+    return render_template('applicants.html', sql_list=sql_list)
 
 if __name__ == "__main__":
     app.secret_key = 'errenincsisszükségám'
--- sql_quaries.py
+++ sql_quaries.py
@@ -9,3 +9,48 @@ def mentors_schools(cursor):
                     order by mentors.id""" )
     result = cursor.fetchall()
     return result
+
+
+@sql_connection.connection_handler
+def all_school(cursor):
+    cursor.execute("""SELECT schools.name,schools.country,first_name,last_name
+                    FROM mentors
+                    JOIN schools
+                    ON mentors.city = schools.city
+                    order by mentors.id""" )
+    result = cursor.fetchall()
+    return result
+
+
+@sql_connection.connection_handler
+def mentors_by_country(cursor):
+    cursor.execute("""SELECT COUNT(schools.country), schools.country
+                    FROM mentors
+                    JOIN schools
+                    ON mentors.city = schools.city
+                    group by schools.country
+                    order by schools.country""" )
+    result = cursor.fetchall()
+    return result
+
+
+@sql_connection.connection_handler
+def contacts(cursor):
+    cursor.execute("""SELECT schools.name, first_name, last_name
+                    FROM mentors
+                    JOIN schools
+                    ON mentors.id = schools.contact_person
+                    order by schools.name""" )
+    result = cursor.fetchall()
+    return result
+
+
+@sql_connection.connection_handler
+def applicants(cursor):
+    cursor.execute("""SELECT first_name, application_code, applicants_mentors.creation_date
+                    FROM applicants
+                    JOIN applicants_mentors
+                    ON applicants.id = applicants_mentors.applicant_id
+                    where applicants_mentors.creation_date > '2016-01-01'""" )
+    result = cursor.fetchall()
+    return result
